eaec2ccb90ff81acb17d508ba6ee43294aaf60e7
karl Gama <kaiquesilva49@hotmail.com>
2021-03-09 19:20:04 -0300

    use memo

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+function calcFactorial(num) {
+    const n = parseInt(num)
+    if(n<0) return -1
+    if(n===0) return 1
+
+    return calcFactorial(n-1) * n
+}
 
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(1)
+    const [factorial,setFactorial] = useState(1)
+    //evita loop infinito
+    useEffect(function() {
+        setFactorial(calcFactorial(number))
+    },[number])
+
+    //ex 2
+    const [status, setStatus] = useState("Ímpar")
+    
+    useEffect(function() {
+        setStatus(number % 2 ===0 ? "Par" : "Ímpar" )
+    }, [number])
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial: </span>
+                    <span className="text red">{factorial}</span>
+                </div>
+                <input type="number" className="input"
+                value={number} onChange={e=>setNumber(e.target.value)}/>
+            </div>
+            <SectionTitle title="exercício 2"/>
+            <div className="center">
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">
+                        {status}
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
